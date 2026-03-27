ed7f075e9a7ba417f3e761fff475579909a5f9aa
Ricardo Crespo <cbergric@gmail.com>
2026-03-27 18:53:26 +0100

    Pequeña reestructuración de cara a varias salas

--- src/motor/MainDos.java
+++ src/motor/MainDos.java
@@ -13,27 +13,35 @@ public class MainDos {
 
     // LLAMAR HEROES 
     List<Personaje> listaHeroes = FabricaHeroes.crearEquipoInicial();
-   
-    // OBTENER SALA 6 DE LA FABRICA
-    System.out.println("Entrando en la Sala 6...");
-    Sala salaActual = FabricaSalas.generarSala(6);
-    List<Personaje> listaEnemigos = salaActual.getEnemigos();
-
     Personaje[] heroes = listaHeroes.toArray(new Personaje[0]);
-    Personaje[] enemigos = listaEnemigos.toArray(new Personaje[0]);
 
-    // MOSTRAR INFO
-    System.out.println("=== HEROES ===");
-    for (Personaje h : heroes) {
-      h.mostrarInfo();
+    int[] ordenSalas = {6}; // TODO: añadir el resto de salas y un random que determine el orden
+
+    for (int numSala : ordenSalas) {
+      if (!MotorCombate.hayVivos(heroes)) {
+        break;
+      }
+
+      // OBTENER SALA 6 DE LA FABRICA
+      System.out.println("Entrando en la Sala " + numSala + "...");
+      Sala salaActual = FabricaSalas.generarSala(numSala);
+      List<Personaje> listaEnemigos = salaActual.getEnemigos();
+
+      Personaje[] enemigos = listaEnemigos.toArray(new Personaje[0]);
+
+      // MOSTRAR INFO
+      System.out.println("=== HEROES ===");
+      for (Personaje h : heroes) {
+        h.mostrarInfo();
+      }
+
+      System.out.println("\n=== ENEMIGOS ===");
+      for (Personaje e : enemigos) {
+        e.mostrarInfo();
+      }
+
+      // EJECUTAR COMBATE
+      MotorCombate.iniciarCombate(heroes, enemigos);
     }
-
-    System.out.println("\n=== ENEMIGOS ===");
-    for (Personaje e : enemigos) {
-      e.mostrarInfo();
-    }
-
-    // EJECUTAR COMBATE
-    MotorCombate.iniciarCombate(heroes, enemigos);
   }
 }
